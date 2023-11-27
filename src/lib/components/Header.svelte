192c7439bd8f46568135840541e49bc8eb396753
Dirk Porsche <dirkporsche78@googlemail.com>
2023-11-27 22:15:14 +0100

    fix: supabase URL

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,8 +3,9 @@
   
   import { dictEntry } from '$lib/conf/translations';
 
-  import { ACCOMADE_USER_ID } from '$lib/conf/settings';
-  export let logoLink:string = `https://ipajhcabityibeaqfzju.supabase.co/storage/v1/object/public/users/${ACCOMADE_USER_ID}/logo.svg`
+  import { ACCOMADE_USER_ID, PUBLIC_SUPABASE_URL } from '$lib/conf/settings';
+
+  export let logoLink:string = `${PUBLIC_SUPABASE_URL}/storage/v1/object/public/users/${ACCOMADE_USER_ID}/logo.svg`
 </script>
 
 <header>
